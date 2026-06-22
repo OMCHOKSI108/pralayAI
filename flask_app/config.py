b0ca9de4ce9fd97c920d824f7585b616ead76bfe
CHOKSI OM CHIRAGBHAI <134709654+OMCHOKSI108@users.noreply.github.com>
2026-06-22 16:50:24 +0530

    Update config.py

--- flask_app/config.py
+++ flask_app/config.py
@@ -1,6 +1,7 @@
 import os
 
-
 class Config:
     SECRET_KEY = os.getenv("FLASK_SECRET", "pralayai-flask-secret-change-me")
+
+    
     API_BASE = os.getenv("PRALAY_API_URL", "http://127.0.0.1:8000")
